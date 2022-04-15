4fe722c9d07e13a421a40e1efefc1225be02cb50
Ethan_Jiang <1766569133@qq.com>
2022-04-14 21:10:11 -0700

    python gui

---
+++ graph4.py
@@ -0,0 +1,33 @@
+import random
+from itertools import count
+import pandas as pd
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+
+plt.style.use('fivethirtyeight')
+
+x_vals = []
+y_vals = []
+
+index = count()
+
+
+def animate(i):
+    data = pd.read_csv('data.csv')
+    x = data['x_value'][-100:]
+    # y1 = data['total_1'][-100:]
+    y2 = data['total_2'][-100:]
+
+    plt.cla()
+
+    # plt.plot(x, y1, label='Channel 1')
+    plt.plot(x, y2, label='Channel 2')
+
+    plt.legend(loc='upper left')
+    plt.tight_layout()
+
+
+ani = FuncAnimation(plt.gcf(), animate, interval=200)
+
+plt.tight_layout()
+plt.show()
